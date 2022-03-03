e82e8343e2fbaa2a5fef1b19df2db99f32b2263c
Karla Abad <kabad1014@gmail.com>
2022-03-03 10:23:16 -0500

    Server running

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const socket = require ("socket.io");
+const port = 8000;
+app.use(cors());
+
+const server = app.listen(port, ()=> { console.log(`Listening on port: ${port}`)});
+
+const io = socket(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["*"],
+        credentials: true,
+    }
+});
+
+io.on("connection", socket => {
+    console.log('socket id: ' + socket.id);
+    console.log("Nice to meet you. (shake hand)")
+    socket.emit("Welcome new Friend!");
+    socket.on("event_from_client", data => {
+        // send a message with "data" to ALL clients EXCEPT for the one that emitted the
+    	//     "event_from_client" event
+        socket.broadcast.emit("event_to_all_other_clients", data);
+    });
+});
+
